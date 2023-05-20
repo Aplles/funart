839919a367121d7ae27206d6c177207479e801a6
SomeBody <maxim.karaichev@saltpepper.ru>
2023-05-20 22:31:50 +0300

    added serializer and outcome.result

--- api/views/category.py
+++ api/views/category.py
@@ -1,7 +1,9 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from service_objects.services import ServiceOutcome
 
+from api.serializers.category.list import CategoryListSerializer
 from api.services.category.list import CategoryListService
 
 
@@ -9,4 +11,4 @@ class CategoryListView(APIView):
 
     def get(self, request, *args, **kwargs):
         outcome = ServiceOutcome(CategoryListService, {})
-        return Response()
+        return Response(CategoryListSerializer(outcome.result, many=True).data, status=status.HTTP_200_OK)
