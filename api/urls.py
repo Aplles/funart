f755093d8bec2001a42b142de18c7df4f7876f6b
aleksandr razarenov <aleksandr.razarenov@saltpepper.ru>
2023-05-21 00:49:55 +0300

    Cherry pick feature/global_search last commit into feature/docs

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from api.views.coloring import ColoringListView
-from api.views.theme import ThemeListView
+from api.views.theme import ThemeListView, ThemeListByCategoryView
 from api.views.category import CategoryListView
-from api.views.theme import ThemeListByCategoryView
+from api.views.coloring import ColoringListView, ColoringListBySearchView
 
 urlpatterns = [
     # Category
@@ -13,4 +12,7 @@ urlpatterns = [
     # Theme
     path('themes/', ThemeListView.as_view()),
     path('themes/<int:id>/colorings/', ColoringListView.as_view()),
+
+    # Search
+    path('search/', ColoringListBySearchView.as_view()),
 ]
